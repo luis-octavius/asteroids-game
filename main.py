c42ff7bf30da99bd634466299992404d2f0e391e
luis-octavius <luisoctavius.sc@gmail.com>
2025-07-24 18:59:43 -0300

    initialize pygame

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    print(f"""
+        Starting Asteroids!
+        Screen width: {SCREEN_WIDTH}
+        Screen height: {SCREEN_HEIGHT}
+    """)
+
+if __name__ == "__main__":
+    main()
