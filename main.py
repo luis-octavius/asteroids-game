566836d0f0985c5ea0e31d506169e941b3ef9298
luis-octavius <luisoctavius.sc@gmail.com>
2025-07-26 17:33:21 -0300

    feat: create sprite groups to separate distinct things and read code better

--- main.py
+++ main.py
@@ -10,24 +10,36 @@ def main():
     """)
 
     pygame.init()
+    
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    print("Drawables: ", drawables)
     # Infinite loop to keep the game up and running
     while True:
         # Quit game in X button
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 return 
-        screen.fill("black") # Background
-        player.draw(screen)
+        updatables.update(dt)
+
+        screen.fill("black")
+
+        for thing in drawables:
+            thing.draw(screen)
+
+        dt = clock.tick(60) / 1000 # FPS limit to 60 - miliseconds to seconds
+
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 # FPS
+        # print("DT: ", dt)
 
 if __name__ == "__main__":
     main()
